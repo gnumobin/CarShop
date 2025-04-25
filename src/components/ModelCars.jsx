be5f826b5542a0b6d9b11a99931da8727e956eed
gnumobin <gnumobin@gmail.com>
2025-04-25 20:41:56 +0330

    Build Vendas

--- src/components/ModelCars.jsx
+++ src/components/ModelCars.jsx
@@ -1,22 +1,40 @@
-import React from "react";
-
-import Pic from "../assets/img/models/1.jpg";
-import { FaChevronRight } from "react-icons/fa";
 import { Link } from "react-router";
 
-const ModelCards = () => {
+const ModelCards = ({ pictures, title }) => {
+  // const pictures = [Pic, Pic, Pic];
+
   return (
     <section className="py-8">
       <div className="container mx-auto px-4">
         {/* Heading */}
         <h2 className="text-xl md:text-2xl lg:text-6xl font-semibold mb-[5rem] text-center md:text-left">
-          Modelos
+          {title}
         </h2>
 
         {/* Cards Container */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          {pictures.map((pic, index) => (
+            <div
+              className="bg-white rounded-[2.5rem] overflow-hidden h-[25rem] md:h-[30rem] relative"
+              key={index}
+            >
+              <img
+                src={pic.src}
+                alt="Car Image"
+                className="w-full md:h-full object-cover"
+              />
+              <div className="px-[4rem] pb-[2rem] absolute bottom-0 flex items-center justify-between w-full">
+                <h3 className="text-base md:text-lg lg:text-4xl font-semibold text-white mb-2 ">
+                  {pic.link}
+                </h3>
+                <Link className="text-white text-[3rem]">
+                  {pic.icon}
+                </Link>
+              </div>
+            </div>
+          ))}
           {/* Card 1: A combustão */}
-          <div className="bg-white rounded-[2.5rem] overflow-hidden h-[25rem] md:h-[30rem] relative">
+          {/* <div className="bg-white rounded-[2.5rem] overflow-hidden h-[25rem] md:h-[30rem] relative">
             <img
               src={Pic}
               alt="Car Image"
@@ -30,10 +48,10 @@ const ModelCards = () => {
                 <FaChevronRight />
               </Link>
             </div>
-          </div>
+          </div> */}
 
           {/* Card 2: Híbridos */}
-          <div className="bg-white rounded-[2.5rem] overflow-hidden h-[25rem] md:h-[30rem] relative">
+          {/* <div className="bg-white rounded-[2.5rem] overflow-hidden h-[25rem] md:h-[30rem] relative">
             <img
               src={Pic}
               alt="Car Image"
@@ -47,10 +65,10 @@ const ModelCards = () => {
                 <FaChevronRight />
               </Link>
             </div>
-          </div>
+          </div> */}
 
           {/* Card 3: Elétrico */}
-          <div className="bg-white rounded-[2.5rem] overflow-hidden h-[25rem] md:h-[30rem] relative">
+          {/* <div className="bg-white rounded-[2.5rem] overflow-hidden h-[25rem] md:h-[30rem] relative">
             <img
               src={Pic}
               alt="Car Image"
@@ -64,7 +82,7 @@ const ModelCards = () => {
                 <FaChevronRight />
               </Link>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,61 @@
-import FeaturedCar from "../components/FeaturedCar"
-import HomeHero from "../components/HomeHero"
-import ModelCars from "../components/ModelCars"
+import FeaturedCar from "../components/FeaturedCar";
+import HomeHero from "../components/HomeHero";
+import ModelCars from "../components/ModelCars";
+// assets
+import ModelPic1 from "../assets/img/models/1.jpg";
+import ModelPic2 from "../assets/img/models/2.jpg";
+import ModelPic3 from "../assets/img/models/3.jpg";
+// assets
+import VendasPic1 from "../assets/img/vendas/1.jpg";
+import VendasPic2 from "../assets/img/vendas/2.jpg";
+import VendasPic3 from "../assets/img/vendas/3.jpg";
+
+import { FaCheck, FaChevronRight } from "react-icons/fa";
 
 function HomePage() {
+  const pictures = [
+    {
+      src: ModelPic1,
+      link: "A combustão",
+      icon: <FaChevronRight />,
+    },
+    {
+      src: ModelPic2,
+      link: "Híbrido",
+      icon: <FaChevronRight />,
+    },
+    {
+      src: ModelPic3,
+      link: "Elétrico",
+      icon: <FaChevronRight />,
+    },
+  ];
+
+  const vendas = [
+    {
+      src: VendasPic1,
+      link: "Ram 1500",
+      icon: <FaCheck />,
+    },
+    {
+      src: VendasPic2,
+      link: "Toyota SW4",
+      icon: <FaCheck />,
+    },
+    {
+      src: VendasPic3,
+      link: "Sea-Doo Gti",
+      icon: <FaCheck />,
+    },
+  ];
   return (
     <div>
-        <HomeHero />
-        <ModelCars />
-        <FeaturedCar />
+      <HomeHero />
+      <ModelCars pictures={pictures} title={"Modelos"} />
+      <FeaturedCar />
+      <ModelCars pictures={vendas} title={"Últimas vendas"} />
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
